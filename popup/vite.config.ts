f41d058acc0c072f6cd0493942572d22964c918d
longxiang.li <lilongxiang2000@gmail.com>
2023-12-21 16:38:15 +0800

    chore: cp manifest.json to 'build/'

--- popup/vite.config.ts
+++ popup/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/build/popup",
+  base: "/popup",
   build: {
     outDir: `${path.resolve(__dirname, "../build/popup")}/`,
     emptyOutDir: true,
